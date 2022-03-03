65fba53deb4c103a988e73f6fe9889b5a803c6bf
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-03-03 10:56:25 -0500

    define login/logout functions, add navbar and styling

--- app.py
+++ app.py
@@ -19,6 +19,29 @@ toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
+@app.before_request
+def add_user_to_g():
+    """If we're logged in, add curr user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
 @app.route('/')
 def home_page():
     '''Shows homepage for app'''
